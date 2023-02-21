b5c03c6fa864d2ea5aedb9e5934aaffb5cf4e503
Conrad Schmidt <conrad@owatz.de>
2023-02-21 10:56:26 +0100

    nullable movie entries

--- src/entity/Movie.ts
+++ src/entity/Movie.ts
@@ -3,7 +3,7 @@ import {Entity, Column, PrimaryColumn} from "typeorm";
 @Entity()
 export class Movie {
 
-    @Column("varchar", { length: 12 })
+    @Column("varchar", { length: 12, nullable: true })
     imdbId: string;
 
     @PrimaryColumn()
@@ -12,7 +12,7 @@ export class Movie {
     @Column()
     name: string;
 
-    @Column("date")
+    @Column("date", {nullable: true})
     releaseDate: Date;
 
     @Column("int")
